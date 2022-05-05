78e72fe1ea77866cf0b2f4e058ee31a4a3a9c04a
Adrian Lugo <misteralugo@gmail.com>
2022-05-04 22:50:29 -0500

    added mobile menu styling

--- index.js
+++ index.js
@@ -4,28 +4,38 @@ const menu = document.querySelector(".menu")
 const closeBtn = document.querySelector(".close-btn")
 const body = document.querySelector(".body")
 const darkener = document.querySelector(".menu-bg-darkener")
+const featuresBtn = document.querySelector(".features-btn")
+const featuresList = document.querySelector(".features-list")
+const companyBtn = document.querySelector(".company-btn")
+const companyList = document.querySelector(".company-list")
 
 window.addEventListener("resize", renderUI)
-menuBtn.addEventListener("click", showMenu)
-closeBtn.addEventListener("click", closeMenu)
+menuBtn.addEventListener("click", 
+    () => toggleMenu("open-menu", "darkener-on"))
+closeBtn.addEventListener("click", 
+    () => toggleMenu("open-menu", "darkener-on"))
+featuresBtn.addEventListener("click",
+    () => toggleMenuList(featuresList, "open-menu-list"))
+companyBtn.addEventListener("click",
+    () => toggleMenuList(companyList, "open-menu-list"))
+
+renderUI()
 
 function renderUI() {
     if (window.innerWidth >= 768) {
         heroImage.src = "./images/image-hero-desktop.png"
         menu.classList.remove("open-menu")
+        darkener.classList.remove("darkener-on")
     } else {
         heroImage.src = "./images/image-hero-mobile.png"
     }
 }
 
-function showMenu() {
-    menu.classList.add("open-menu")
-    darkener.classList.add("darkener-on")
+function toggleMenu(menuClass, darkenerClass) {
+    menu.classList.toggle(menuClass)
+    darkener.classList.toggle(darkenerClass)
 }
 
-function closeMenu() {
-    menu.classList.remove("open-menu")
-    darkener.classList.remove("darkener-on")
+function toggleMenuList(target, listClass) {
+    target.classList.toggle(listClass)
 }
-
-renderUI()
