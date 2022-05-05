66a857e2fecd4d2388ef6343721e521b6871042a
Adrian Lugo <misteralugo@gmail.com>
2022-05-04 20:40:19 -0500

    added menu open close functionality and dark bg

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-window.addEventListener("resize", renderUI)
 const heroImage = document.querySelector(".hero-image").children[0]
+const menuBtn = document.querySelector(".menu-btn")
+const menu = document.querySelector(".menu")
+const closeBtn = document.querySelector(".close-btn")
+const body = document.querySelector(".body")
+const darkener = document.querySelector(".menu-bg-darkener")
 
+window.addEventListener("resize", renderUI)
+menuBtn.addEventListener("click", showMenu)
+closeBtn.addEventListener("click", closeMenu)
 
 function renderUI() {
-    if (window.innerWidth >= 768 &&
-        heroImage.src !== "./images/image-hero-desktop.png") {
-            heroImage.src = "./images/image-hero-desktop.png"
+    if (window.innerWidth >= 768) {
+        heroImage.src = "./images/image-hero-desktop.png"
+        menu.classList.remove("open-menu")
     } else {
         heroImage.src = "./images/image-hero-mobile.png"
     }
+}
+
+function showMenu() {
+    menu.classList.add("open-menu")
+    darkener.classList.add("darkener-on")
+}
+
+function closeMenu() {
+    menu.classList.remove("open-menu")
+    darkener.classList.remove("darkener-on")
 }
 
 renderUI()
